ea4be38ff23935af78f1e610dbbe046dd5c01447
James Donovan <james.l.donovan@gmail.com>
2022-12-15 09:22:55 -0600

    Test Update

--- TestFunctionApp/Function1.cs
+++ TestFunctionApp/Function1.cs
@@ -25,6 +25,8 @@ namespace TestFunctionApp
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
+            log.LogInformation("This is a test");
+
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
